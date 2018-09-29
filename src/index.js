7fa350c42519857544c348236c8f1e13a58a55b1
Mariya <37655379+MariyaPolt@users.noreply.github.com>
2018-09-29 16:29:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  var stack = [];
+  let stack = [];
 
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < bracketsConfig.length; j++) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
       if (str[i] == bracketsConfig[j][0]) {
         if (bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][1] == stack[stack.length - 1]) {
           stack.pop();
